e1fdbcee137fec1656ecb8df26887eea89751c4e
stitrago <punpun.siwakorn@gmail.com>
2025-10-17 13:02:14 +0700

    change all dup name to a single global name and Infinite_Hotel's func name to snake case (again I for got to save change before committing)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
 import tracemalloc
-import HilbertHotel as HH
+import Infinite_Hotel as HH
 
 print('Welcome to Hilbert Hotel')
 initial = int(input("Enter the initial guest amount : "))
 hotel = HH.Infinite_Hotel(initial)
 
 # with open("log.csv", "w", encoding="utf-8") as f:
-#         f.write("log, type, room number\n")
+#         f.write("log, type, room room_id\n")
         
 while True:
     print('-------------------------------------------------------------------------------------------')
@@ -17,10 +17,10 @@ while True:
     print()
     
     if ins[0] == 'HELP': 
-        print(' -> "AG <channel,amount|...>" to add guest. Ex AG Car,10|Bus,6 .')
-        print(' -> "AR <Room_number>" to add that room.')
-        print(' -> "SER <Room_number>" to search that room.')
-        print(' -> "RR <Room_number>" to remove that room')
+        print(' -> "AG <method,amount|...>" to add guest. Ex AG Car,10|Bus,6 .')
+        print(' -> "AR <room_id>" to add that room.')
+        print(' -> "SER <room_id>" to search that room.')
+        print(' -> "RR <room_id>" to remove that room')
         print(' -> "SOR" to sort room and export to csv')
         print(' -> "S" to save data to csv')
         print(' -> "END" to end the program')
@@ -32,40 +32,40 @@ while True:
     tracemalloc.start()
         
     if ins[0] == 'AG':
-        channel_list = []
+        method_list = []
         amount_list = []
         try: 
             for ch in ins[1].split('|'):
-                channel, amount = ch.split(',')
-                channel_list.append(channel)
+                method, amount = ch.split(',')
+                method_list.append(method)
                 amount_list.append(int(amount))
         except:
             print('wrong input type!!!')
             continue
-        hotel.inputGuest(channel_list, amount_list)
+        hotel.inputGuest(method_list, amount_list)
             
     elif ins[0] == 'AR':
         try: 
-            number = int(ins[1])
+            room_id = int(ins[1])
         except:
             print('wrong input type!!!')
             continue
-        if hotel.addRoom(number):
-            print(f'Add room {number} succesfully')
+        if hotel.add_room(room_id):
+            print(f'Add room {room_id} succesfully')
         else:
             print('This room is already exist!!!')
 
     elif ins[0] == 'SER':
         try: 
-            number = int(ins[1])
+            room_id = int(ins[1])
         except:
             print('wrong input type!!!')
             continue
-        room = hotel.search_room_by_id(number)
+        room = hotel.search_room_by_id(room_id)
         if room is None:
             print("This room doesn't exist!!!")
         else:
-            print(f'Room {number}, channel {room.method}({room.method_id})')
+            print(f'Room {room_id}, method {room.method}({room.method_id})')
     
     elif ins[0] == 'SOR':
         hotel.rooms_to_csv()
@@ -73,12 +73,12 @@ while True:
     
     elif ins[0] == 'RR':
         try: 
-            number = int(ins[1])
+            room_id = int(ins[1])
         except:
             print('wrong input type!!!')
             continue
-        if hotel.removeRoom(number):
-            print(f'Remove room {number} succesfully')
+        if hotel.removeRoom(room_id):
+            print(f'Remove room {room_id} succesfully')
         else:
             print("This room doesn't exist!!!")
             
